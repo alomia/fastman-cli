d5e027943acf6c5345a0fdece94518ce3bb12523
Alejandro Alomia <alejandroalomia730@gmail.com>
2023-11-13 00:39:17 -0500

    root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2023 Alejandro Alomia github.com/alomia
-
-*/
 package cmd
 
 import (
@@ -10,11 +6,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "fastman-cli",
+	Use:   "fastman",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -37,15 +31,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fastman-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
